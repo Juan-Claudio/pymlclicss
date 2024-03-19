27f490f2d37be925e8cc041261d3b187b0de6056
Juan-Claudio <angeelric@gmail.com>
2024-03-19 13:13:41 +0100

    change minor to val trads and inheritance

--- pycss_inheritance.py
+++ pycss_inheritance.py
@@ -1,6 +1,30 @@
+from pycss_val_trads import Convert
 from default_values import default_values
-def pyml_to_pycli(pydom):
-    #get root pycss info
-    curr_pycss = {}
-    curr_pycss.update(default_values['pyml'])
-    curr_pycss.update(pydom.get_root().getstyle())
+
+screen = ""
+
+def dom_to_cli(pydom, node=None):
+   #inicialization
+   if node is None:
+      node = pydom.getroot()
+
+   #define the final pycss rules of the current node
+   pycss = {}
+   pycss.update(default_values[node.tag_name])
+   pycss.update(node.getstyle())
+   
+   #css to cli
+   #if border or/and background fill/stroke rect
+   screen += Pycli.rect(
+      Convert.value(pycss['x'], 'x'),
+      Convert.value(pycss['y'], 'y'),
+      Convert.value(pycss['w'], 'w'),
+      Convert.value(pycss['h'], 'h'),
+      Convert.border_style(pycss['bo-s']),
+      pycss['bo-c'],
+      pycss['bg']
+   )
+
+   #loop recursively through pydom
+   for child in node:
+      pass
